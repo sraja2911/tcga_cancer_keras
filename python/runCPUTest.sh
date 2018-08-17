b1fed121363b24f761926bb5aa8f70c33118bc39
Raj Manian <sraja2911@gmail.com>
2018-08-17 11:35:34 -0400

    InceptionV3_runtime
    InceptionV3_runtime summary with ModelC-1024, ModelE-10000

--- python/runCPUTest.sh
+++
@@ -1,8 +0,0 @@
-docker run \
-    --runtime=nvidia \
-    --rm \
-    -ti \
-    -v "${PWD}:/app" \
-    tensorflow/tensorflow:latest-gpu \
-    python /app/benchmark.py gpu 10000 
-
--- python/tcga_simpsons_1024_nvidia.sh
+++ python/tcga_simpsons_1024_nvidia.sh
@@ -3,6 +3,6 @@ nvidia-docker run \
      -v "${PWD}:/app:rw" \
      -v "/home/raj/github/tcga_cancer_ml_classifier/python:/data/code:rw" \
      -v "/home/raj/github/tcga_cancer_ml_classifier/python/results:/data/output/results:rw" \
-     -v "/home/raj/tcgaImageSet_1024/train:/data/train:rw" \
-     -v "/home/raj/tcgaImageSet_1024/test:/data/test:rw" \
-     fgiuste/neuroml:V3
+     -v "/home/raj/tcga_6class_1024_10k/train:/data/train:rw" \
+     -v "/home/raj/tcga_6class_1024_10k/test:/data/test:rw" \
+     fgiuste/neuroml:py3
